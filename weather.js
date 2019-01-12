acb657d6342b08d8317bb5106bfff28c83266321
Richard Ellery <raellery@gmail.com>
2019-01-12 18:29:17 +0900

    Call Dark Sky API with proxy

--- weather.js
+++ weather.js
@@ -7,6 +7,18 @@ window.addEventListener('load', () => {
       console.log(position);
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
+      
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const api = `${proxy}https://api.darksky.net/forecast/4e38f510a79dde73e99fcfe03980e309/${latitude},${longitude}`;
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(response => {
+          let data = response;
+          console.log(data);
+        }); // close fetch(api)
     }); // close getCurrentPosition
   } // close navigator.geolocation
 }); // close window.addEventListener
