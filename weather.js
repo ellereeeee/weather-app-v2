4885f761ebd469851ae1b99d31403fb4a25ae97c
Richard Ellery <raellery@gmail.com>
2019-01-12 19:22:44 +0900

    Render current weather info

--- weather.js
+++ weather.js
@@ -2,14 +2,17 @@ window.addEventListener('load', () => {
   let latitude;
   let longitude;
   
+  let current_weather = document.getElementById('current-weather');
+  let current_wind_speed = document.getElementById('current-wind-speed');
+  let current_precip_chance = document.getElementById('current-precip-chance');
+  
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
       
       const proxy = 'https://cors-anywhere.herokuapp.com/';
-      const api = `${proxy}https://api.darksky.net/forecast/4e38f510a79dde73e99fcfe03980e309/${latitude},${longitude}`;
+      const api = `${proxy}https://api.darksky.net/forecast/4e38f510a79dde73e99fcfe03980e309/${latitude},${longitude}?units=auto`;
 
       fetch(api)
         .then(response => {
@@ -18,6 +21,13 @@ window.addEventListener('load', () => {
         .then(response => {
           let data = response;
           console.log(data);
+          
+          // unpack current weather and render info
+          let { icon, temperature, windSpeed, precipProbability } = data.currently;
+          current_weather.innerHTML = temperature;
+          current_wind_speed.innerHTML = windSpeed;
+          current_precip_chance.innerHTML = precipProbability + '%';
+        
         }); // close fetch(api)
     }); // close getCurrentPosition
   } // close navigator.geolocation
