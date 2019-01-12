2b108f2266fb45f1231c69d36a7bfb39be9217af
Richard Ellery <raellery@gmail.com>
2019-01-12 18:28:51 +0900

    Get current latitude and longitude

--- weather.js
+++ weather.js
@@ -5,6 +5,8 @@ window.addEventListener('load', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       console.log(position);
-    });
-  }
-});
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+    }); // close getCurrentPosition
+  } // close navigator.geolocation
+}); // close window.addEventListener
